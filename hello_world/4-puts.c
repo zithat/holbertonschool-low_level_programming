1ec9fcd5c03f9e46133f5cea0f81abb362ec9a96
root <mrluisito5@gmail.com>
2023-06-20 14:31:20 -0700

    4. Hello, puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - prints "Programming is like building a multilingual
+ *  puzzle, followed by a newline.
+ * Retourns : Always 0.
+ */
+
          int main(void){
 	
 	 puts("\"Programming is like building a multilingual puzzle");
-	return 0; 
+	return 0;
+
  }
