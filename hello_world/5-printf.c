8e8e3e9b1856613a54a98d590b70326f0b6f1986
root <mrluisito5@gmail.com>
2023-06-22 11:26:18 -0700

    Betty corrections

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
-int main(void) {
-    printf("with proper grammar, but the outcome is a piece of art,\n");
-    return 0;
+/**
+ * main - Print a message on the program "with proper grammar, but the outcome
+ *        is a piece of art,"
+ * Return: Integer value 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
+
