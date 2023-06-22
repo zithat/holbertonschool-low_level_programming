35f1099b62b2f5991ef3dfddfb05c0b57e615b78
root <mrluisito5@gmail.com>
2023-06-22 11:54:49 -0700

    Betty corrections

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
+
 /**
- * main - Generates and  prints the size of various types on the computer
- * return : Integer value 0
+ * main - Generates and prints the size of various types on the computer
+ *
+ * Return: Integer value 0
  */
-
-
 int main(void)
 {
-  printf("Size of a char: %lu byte(s)\n", sizeof(char));
-  printf("Size of an int: %lu byte(s)\n", sizeof(int));
-  printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
-  printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
-  printf("Size of a float: %lu byte(s)\n", sizeof(float));
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 
-  return(0);
+	return (0);
 }
+
