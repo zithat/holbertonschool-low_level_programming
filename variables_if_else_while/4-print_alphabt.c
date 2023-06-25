7d8c9888354057e3c014319932c833c2c3faf161
root <mrluisito5@gmail.com>
2023-06-25 14:39:36 -0700

    Alphabet with putchat except letters q and e

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the alphabet in lowercase, excluding 'q' and 'e', followed by a newline.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'q' && letter != 'e')
+			putchar(letter);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
