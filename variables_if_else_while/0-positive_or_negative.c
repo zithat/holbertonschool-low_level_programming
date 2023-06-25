58459c370907785c7f00c041d51760f2b411b059
root <mrluisito5@gmail.com>
2023-06-25 13:47:43 -0700

    Positive or negative exercise

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,27 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* Int N - Used for a random number */
 
-/* Using if to know if the ramdon number is true or false */
-
-int main() {
+/**
+ * main - Entry point
+ *
+ * Description: Generates a random number and checks if it's positive, negative, or zero.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
     int n;
-    
+
     srand(time(0));
     n = rand() - RAND_MAX / 2;
-    
-    if (n > 0) {
+
+    if (n > 0)
+    {
         printf("%d is positive\n", n);
     }
-    
-    if (n < 0) {
+
+    if (n < 0)
+    {
         printf("%d is negative\n", n);
     }
-    
-    if (n == 0) {
+
+    if (n == 0)
+    {
         printf("%d is zero\n", n);
     }
-    
+
     return 0;
 }
+
